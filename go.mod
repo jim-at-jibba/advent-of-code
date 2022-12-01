0bdac5b75312a81b560bc8713bce416957faa3dc
James Best <jim@justjibba.net>
2022-12-01 11:05:57 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jim-at-jibba/advent-of-code
+
+go 1.19
+
+require golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
---
+++ scripts/cmd/prompt/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/jim-at-jibba/advent-of-code/scripts/aoc"
+
+func main() {
+	day, year, cookie := aoc.ParseFlags()
+	aoc.GetPrompt(day, year, cookie)
+}
